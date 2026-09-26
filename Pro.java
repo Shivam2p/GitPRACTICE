cc5c22886b9ed3d22043784f1c4861480ae732c3
Shivam Pandey <122889419+Shivam2p@users.noreply.github.com>
2026-09-26 11:53:46 +0530

    Create Pro.java with a Hello World progra

---
+++ Pro.java
@@ -0,0 +1,9 @@
+public class Pro {
+    
+    // The main method serves as the entry point for your application.
+    public static void main(String[] args) {
+        
+        // Prints the text to the console screen
+        System.out.println("Hello, World!");
+    }
+}
